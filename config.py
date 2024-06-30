21df163cf912b83407a88f7ed4ee5124ef37bfae
Dustin Smith <whoisdsmith@gmail.com>
2024-06-30 01:34:36 -0400

    chore: Remove unused JSON files and update API token in config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_token = '24a3683a-086f-4388-9602-88c479379ba3'
+api_token = 'your_actual_api_token_here'
 headers = {
     'Authorization': f'Bearer {api_token}',
     'Content-Type': 'application/json'
